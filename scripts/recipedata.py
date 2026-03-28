9977e32a798e9dd99e0e082812f45dbec35ae7c5
driftbluestone <driftbluestone@gmail.com>
2026-03-27 17:24:39 -0700

    rewrite recipe data json fragmenter

--- scripts/recipedata.py
+++ scripts/recipedata.py
@@ -7,21 +7,38 @@ with open(f"{DIR}/temp/index.json") as file:
     recipes = json.load(file)
 length = len(recipes)
 for recipe in recipes:
-    for output in recipe["outputs"]:
-        output = list(output.values())[0]
-        items[output.replace(":", "__")] = items.get(output.replace(":", "__"), {})
-        items[output.replace(":", "__")]["output"] = items[output.replace(":", "__")].get("output", {})
-        items[output.replace(":", "__")]["output"][recipe["category"].replace(":", "__")] = items[output.replace(":", "__")]["output"].get(recipe["category"].replace(":", "__"), [])
-        items[output.replace(":", "__")]["output"][recipe["category"].replace(":", "__")].append(recipe)
-    for input in recipe["inputs"]:
-        id, input = list(input.items())[0]
-        if id == "id":
-            items[input.replace(":", "__")] = items.get(input.replace(":", "__"), {})
-            items[input.replace(":", "__")]["input"] = items[input.replace(":", "__")].get("input", {})
-            items[input.replace(":", "__")]["input"][recipe["category"].replace(":", "__")] = items[input.replace(":", "__")]["input"].get(recipe["category"].replace(":", "__"), [])
-            items[input.replace(":", "__")]["input"][recipe["category"].replace(":", "__")].append(recipe)
-        
-for k,v in items.items():
+    recipe_data = {}
+    recipe_data["id"] = recipe["id"]
+    recipe_data["category"] = recipe["category"]
+    recipe_data["dimensions"] = [recipe["width"], recipe["height"]]
+    try:
+        recipe_data["eu_t"] = recipe["euPerTick"]
+    except KeyError:
+        recipe_data["eu_t"] = 0
+    recipe_data["inputs"] = recipe["inputs"]
+    recipe_data["outputs"] = recipe["outputs"]
+    try:
+        recipe_data["catalysts"] = recipe["catalysts"]
+    except KeyError:
+        recipe_data["catalysts"] = []
+    for i in recipe["inputs"]:
+        for k, v in i.items():
+            if k != "id": continue
+            v = v.replace(":", "__")
+            items[v] = items.get(v, {})
+            items[v]["uses"] = items[v].get("uses", {})
+            items[v]["uses"][recipe_data["category"]] = items[v]["uses"].get(recipe_data["category"], [])
+            items[v]["uses"][recipe_data["category"]].append(recipe_data)
+    for i in recipe["outputs"]:
+        for k, v in i.items():
+            if k != "id": continue
+            v = v.replace(":", "__")
+            items[v] = items.get(v, {})
+            items[v]["recipes"] = items[v].get("recipes", {})
+            items[v]["recipes"][recipe_data["category"]] = items[v]["recipes"].get(recipe_data["category"], [])
+            items[v]["recipes"][recipe_data["category"]].append(recipe_data)
+ 
+for k, v in items.items():
     with open(f"{DIR}/../recipedata/{k}.json", "w") as file:
         json.dump(v, file, indent=2)
 
